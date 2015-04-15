6d1f83ee3b2f017d55a6e40054ccaff440c5e6bb
shakked <zshakked@stevens.edu>
2015-04-15 14:19:37 -0400

    Added throwabiltiy to ZSSBall class
    Made it very easy to add 'throwable' behavior to ZSSBall IF desired. It is not mandatory.

--- Zachary Shakked/ZSSBall.swift
+++ Zachary Shakked/ZSSBall.swift
@@ -10,12 +10,76 @@ import UIKit
 
 class ZSSBall: UIView {
     var radius : CGFloat = 0;
-    var interactivityEnabled : Bool = false
+    var interactivityEnabled : Bool = true
+    var physicsEnabled : Bool = true
+    
+    private var gravity : UIGravityBehavior?
+    private var dynamics : UIDynamicItemBehavior?
+    private var collision : UICollisionBehavior?
+    
+    private var lastTouchLocation : CGPoint = CGPoint(x: 0.0, y: 0.0)
+    private var lastTouchTime : NSDate = NSDate()
+    private var currentTouchVelocity : CGPoint = CGPoint(x: 0.0, y: 0.0)
+
     
     override func drawRect(rect: CGRect) {
         super.drawRect(rect)
         layer.cornerRadius = rect.width / 2.0
         layer.masksToBounds = true
+    }
+    
+    func addVelocity(velocity: CGPoint) -> Void {
+        dynamics?.addLinearVelocity(velocity, forItem: self)
+    }
+    
+    func disablePhysics() -> Void {
+        disableGravity()
+        disableDynamics()
+        disableCollision()
+        physicsEnabled = false
+    }
+    
+    func enablePhysics() -> Void {
+        enableGravity()
+        enableDynamics()
+        enableCollision()
+        physicsEnabled = true
+    }
+    
+    func enableGravity() -> Void {
+        if let gravity = gravity {
+            gravity.addItem(self)
+        }
+    }
+    
+    func disableGravity() -> Void {
+        if let gravity = gravity {
+            gravity.removeItem(self)
+        }
+    }
+    
+    func enableDynamics() -> Void {
+        if let dynamics = dynamics {
+            dynamics.addItem(self)
+        }
+    }
+    
+    func disableDynamics() -> Void {
+        if let dynamics = dynamics {
+            dynamics.removeItem(self)
+        }
+    }
+    
+    func enableCollision() -> Void {
+        if let collision = collision {
+            collision.addItem(self)
+        }
+    }
+    
+    func disableCollision() -> Void {
+        if let collision = collision {
+            collision.removeItem(self)
+        }
     }
     
     override init(frame: CGRect) {
@@ -25,6 +89,13 @@ class ZSSBall: UIView {
     
     required init(coder aDecoder: NSCoder) {
         fatalError("NSCoder not supporter")
+    }
+    
+    func configurePhysics(#gravity: UIGravityBehavior, dynamics: UIDynamicItemBehavior, collision: UICollisionBehavior) -> Void {
+        self.gravity = gravity
+        self.dynamics = dynamics
+        self.collision = collision
+        enablePhysics()
     }
     
     convenience init(radius: CGFloat, center: CGPoint) {
@@ -44,13 +115,48 @@ class ZSSBall: UIView {
         self.radius = radius
     }
     
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        var theTouches = touches as NSSet
+        let touch = theTouches.anyObject()! as! UITouch
+        let touchLocation = touch.locationInView(self.superview)
+        
+        disablePhysics()
+    }
+    
     override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
         if interactivityEnabled {
+            
             var theTouches = touches as NSSet
             let touch = theTouches.anyObject()! as! UITouch
             let touchLocation = touch.locationInView(self.superview!)
             self.center = touchLocation
         }
+        
+        calculateAndSetTouchVelocity()
     }
+    
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        enablePhysics()
+        if interactivityEnabled {
+            addVelocity(currentTouchVelocity)
+        }
+    }
+    
+    func calculateAndSetTouchVelocity() -> Void {
+        let currentTouchLocation = self.center
+        
+        let xDistance = currentTouchLocation.x - lastTouchLocation.x
+        let yDistance = currentTouchLocation.y - lastTouchLocation.y
+        
+        let timeSinceLastTouch = NSDate().timeIntervalSinceDate(lastTouchTime)
+        
+        let touchVelocityX = Double(xDistance) / Double(timeSinceLastTouch)
+        let touchVelocityY = Double(yDistance) / Double(timeSinceLastTouch)
+        currentTouchVelocity = CGPoint(x: touchVelocityX, y: touchVelocityY)
+        lastTouchTime = NSDate()
+        lastTouchLocation = currentTouchLocation
+    }
+    
+    
 
 }
--- Zachary Shakked/ZSSHomeViewController.swift
+++ Zachary Shakked/ZSSHomeViewController.swift
@@ -23,6 +23,7 @@ class ZSSHomeViewController: UIViewController {
     var touchVelocityY : Float = 0.0
     var timeOfLastTouch = NSDate(timeIntervalSince1970: 0)
     var locationOfLastTouch = CGPoint(x: 10, y: 10)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureAnimators()
@@ -34,10 +35,11 @@ class ZSSHomeViewController: UIViewController {
         animator = UIDynamicAnimator(referenceView: view)
         
         gravity = UIGravityBehavior()
-        gravity.magnitude = 0.5
+        gravity.magnitude = 1.0
         
         collision = UICollisionBehavior()
         collision.translatesReferenceBoundsIntoBoundary = true
+
         
         itemBehavior = UIDynamicItemBehavior()
         itemBehavior.elasticity = 0.9
@@ -54,77 +56,16 @@ class ZSSHomeViewController: UIViewController {
     }
     
     func showBalls() -> Void {
-        greenBall = ZSSBall(radius: 10, center: CGPointMake(50, 50), color: UIColor.greenColor())
-        blueBall = ZSSBall(radius: 25, center: CGPointMake(200, 200), color: UIColor.blueColor())
-//        let pan = UIPanGestureRecognizer(target: self, action: Selector("panRecognized:"))
-//        self.view.addGestureRecognizer(pan)
         
-        blueBall.interactivityEnabled = true
-        view.addSubview(greenBall)
+        blueBall = ZSSBall(radius: 25, center: CGPointMake(200, 200), color: UIColor.blueColor())
+
         view.addSubview(blueBall)
         
-        gravity.addItem(greenBall)
-        gravity.addItem(blueBall)
         
-        collision.addItem(greenBall)
-        collision.addItem(blueBall)
-        
-        itemBehavior.addItem(greenBall)
-        itemBehavior.addItem(blueBall)
-    }
-    
-    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-        var theTouches = touches as NSSet
-        let touch = theTouches.anyObject()! as! UITouch
-        let touchLocation = touch.locationInView(self.view)
-        
-        let blueBallFrame = blueBall.frame
-        if blueBallFrame.contains(touchLocation) {
-            gravity.removeItem(blueBall)
-            collision.removeItem(blueBall)
-            itemBehavior.removeItem( blueBall)
-            gravityHasBlueBall = false
-        }
-    }
-    
-    func panRecognized(sender: UIPanGestureRecognizer) -> Void {
+        blueBall.configurePhysics(gravity: gravity, dynamics: itemBehavior, collision: collision)
         
     }
     
-    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
-        var theTouches = touches as NSSet
-        let touch = theTouches.anyObject()! as! UITouch
-        let touchLocation = touch.locationInView(self.view)
-        
-        let lastTouchX = locationOfLastTouch.x
-        let lastTouchY = locationOfLastTouch.y
-        let currentTouchX = touchLocation.x
-        let currentTouchY = touchLocation.y
-        
-        let xDistance = currentTouchX - lastTouchX
-        let yDistance = currentTouchY - lastTouchY
-        
-        let xFloat = Float(xDistance)
-        let yFloat = Float(yDistance)
- 
-        let timeSinceLastTouch = NSDate().timeIntervalSinceDate(timeOfLastTouch)
-        let timeFloat : Float = Float(timeSinceLastTouch)
-        
-        touchVelocityX = xFloat / timeFloat
-        touchVelocityY = yFloat / timeFloat
-        timeOfLastTouch = NSDate()
-        locationOfLastTouch = touchLocation
-    }
-    
-    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
-        if !gravityHasBlueBall {
-            gravity.addItem(blueBall)
-            collision.addItem(blueBall)
-            itemBehavior.addItem(blueBall)
-            itemBehavior.addLinearVelocity(CGPointMake(CGFloat(touchVelocityX), CGFloat(touchVelocityY)), forItem: blueBall)
-            gravityHasBlueBall = true
-        }
-    }
 
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
